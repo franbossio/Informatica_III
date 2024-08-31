2ad24fe608ba1838dfcbe231f73bfedf6b886e4a
Francisco Bossio <franciscobossio10@gmail.com>
2024-08-31 16:51:11 -0300

    borrador terminado

--- Ordenamiento/src/Main.java
+++ Ordenamiento/src/Main.java
@@ -6,18 +6,17 @@ public class Main {
         List<Pedido> pedido=new ArrayList<>();
         Scanner consola=new Scanner(System.in);
         int op;
+        Pizzeria pizzeria = new Pizzeria();
         Ordenador ordenar= new Ordenador();
-        pedido.add(new Pedido("a", 10, 40));
-        pedido.add(new Pedido("c", 60, 50));
-        pedido.add(new Pedido("d", 25, 10));
-        pedido.add(new Pedido("b", 40, 15));
        do { 
             System.out.println("Elige una opcion:");
             System.out.println("1-Ordenar por timepo");
             System.out.println("2-Ordenar por precio total");
             System.out.println("3-Ordenar por nombre del cliente");
             System.out.println("4-Mostrar lista de pedidos");
-            System.out.println("5-Salir del programa");
+            System.out.println("5-Agregar pedido");
+            System.out.println("6-Eliminar pedido");
+            System.out.println("7-Salir del sistema");
             op=consola.nextInt();
             switch (op) {
                 case 1:
@@ -34,8 +33,15 @@ public class Main {
                     System.out.println(e);
                 }
                     break;
+                case 5:
+                    pizzeria.agregarPedido(pedido);
+                    break;
+                case 6: 
+                    pizzeria.eliminarPedido(pedido);
+                    break;
+
             }
-        } while (op!=5);
+        } while (op!=7);
         
 
     }
--- Ordenamiento/src/Pizzeria.java
+++ Ordenamiento/src/Pizzeria.java
@@ -1,3 +1,27 @@
+import java.util.*;
 public class Pizzeria {
+
+    public Pizzeria(){}
+
+    public void agregarPedido(List<Pedido> pedido){
+        System.out.println("Ingresar la cantidad de pedidos para agregar:");
+        int n = new Scanner(System.in).nextInt();
+        Random random = new Random();
+        String[] nombres = {"Juan", "Ana", "Carlos", "Maria", "Luis", "Elena", "Pedro", "Laura", "Miguel", "Carmen"};
+        for (int i = 0; i < n; i++) {
+            String nombreCliente = nombres[random.nextInt(nombres.length)];
+            int precioTotal = (random.nextInt(500-100+1)+10); // Precio entre 100 y 500
+            int tiempoPreparacion = (random.nextInt(60 - 10 + 1 )+10); 
+
+            pedido.add(new Pedido(nombreCliente, precioTotal, tiempoPreparacion));
+        }
+    }
     
+    public void eliminarPedido(List<Pedido> pedido){
+        System.out.println("Ingrese el numero de pedido:");
+        int n = new Scanner(System.in).nextInt();
+        pedido.remove(n-1);
+    }
+
 }
+
