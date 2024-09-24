1f85836887c752fe6d24294733ae4c380af34474
Francisco Bossio <franciscobossio10@gmail.com>
2024-09-24 17:26:31 -0300

    archivo actualizado

---
+++ CodigoClases/src/Nodo.java
@@ -0,0 +1,9 @@
+public class Nodo {
+    public int element;
+    public Nodo next;
+
+    public Nodo(int element){
+        this.element=element;
+        next=null;
+    }
+}
--- CodigoClases/src/StackList.java
+++ CodigoClases/src/StackList.java
@@ -1,15 +1,36 @@
 public class StackList {
-    private StackNodo<Integer> topOfStack = null;
+    private Nodo topOfStack;
+
+    public StackList(){
+        topOfStack=null;
+    }
 
     public void push(int x){
-        if(isEmpty()){
-            new UnderflowException("stack vacio");
-        }else{
-        }
+        topOfStack=new Nodo(x);
     }
 
     public boolean isEmpty(){
         return topOfStack==null;
+    }
+
+    public void pop(){
+        if(isEmpty()){
+            throw new UnderflowException("Stack empty");
+        }else{
+            topOfStack=topOfStack.next;
+        }
+    }
+
+    public int top(){
+
+    }
+
+    public int topAndPop(){
+
+    }
+
+    public void makeEmpty(){
+        topOfStack=null;
     }
 
 }
