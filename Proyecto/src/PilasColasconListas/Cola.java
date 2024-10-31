de7b445f68b47f3eeda5a20f4e253aaeb8b15a78
Francisco Bossio <franciscobossio10@gmail.com>
2024-10-31 18:18:04 -0300

    archivos actualizados

--- Proyecto/src/PilasColasconListas/Cola.java
+++ Proyecto/src/PilasColasconListas/Cola.java
@@ -8,4 +8,63 @@ public class Cola<T> {
         this.front=null;
         this.back=null;
     }
+
+    //ingresar un valor a la cola
+    public void enqueue(T x){
+        Nodo<T> new_Nodo = new Nodo<>(x);
+        if(isEmpty()){   
+            back=new_Nodo;
+            front=back;
+        }else{
+            back.next = new_Nodo;  // Aquí apuntas el siguiente nodo de 'back' al nuevo nodo
+            back = new_Nodo;  // Luego, actualizas el 'back' al nuevo nodo
+        }
+    }
+
+    public boolean isEmpty(){
+        return front==null && back==null;
+    }
+
+    //eleminar y mostrar el valor eliminado 
+    public T dequeue(){
+        if(isEmpty()){
+            throw new UnderflowException("queue empty");
+        }else{
+            T aux = front.dato;
+            front=front.next;
+            if (front == null) {
+                back = null;
+            }
+            return aux;
+        }
+    }
+
+    //obtener el frente (primer valor ingresado) de la lista
+    public T getFront() {
+        if(isEmpty()){
+            throw new UnderflowException("queue empty");
+        }else{
+            return front.dato;
+        }
+       
+    }
+
+    //mostrar la cola completa
+    public void mostrarCola(){
+        Nodo<T> aux_Nodo=front;
+        if(isEmpty()){
+            throw new UnderflowException("queue empty");
+        }else{
+            while (aux_Nodo!=null) {
+                System.out.println(aux_Nodo.dato);
+                aux_Nodo=aux_Nodo.next;
+            }
+        }
+    }
+
+    //hacer vacia la cola
+    public void makeEmpty(){
+        front = null;
+        back = null;
+    }
 }
--- Proyecto/src/PilasColasconListas/EjercicioPilasColasconLista.java
+++ Proyecto/src/PilasColasconListas/EjercicioPilasColasconLista.java
@@ -5,6 +5,8 @@ public class EjercicioPilasColasconLista {
 
     Pila<Integer> pilaEntero = new Pila<>();
     Pila<String> pilaString = new Pila<>();
+    Cola<Integer> colaEntero = new Cola<>();
+
     Scanner consola = new Scanner(System.in);
     int op, x, op2;
 
@@ -13,36 +15,63 @@ public class EjercicioPilasColasconLista {
         System.out.println("--------Pilas y colas con listas--------");
         System.out.println("Elegir el ejercicio: ");
         System.out.println("1-Implementación de una Pila con lista");
-        System.out.println("2-Implementa una pila que almacene palabras. Luego, desapílalas e imprímelas en orden inverso.");
-        System.out.println("3-Verificacion de parentesis");
+        System.out.println("2-Implementa una pila que almacene palabras. Luego, desapílalas e imprímelas en orden inverso");
+        System.out.println("3-Verificacion de parentesis con pilas");
         System.out.println("4-Convertir decimal a binario");
-        System.out.println("5-Salir");
+        System.out.println("5-Implementación de una Cola con lista");
+        System.out.println("6-Implementa una cola que almacene números enteros. Luego, desencola y suma todos los números");
+        System.out.println("7-Ordenamiento de Cola");
+        System.out.println("8-Verificación de Palíndromos con cola");
+        System.out.println("9-Salir");
         op = consola.nextInt();
         switch (op) {
             case 1:
             System.out.println("----------Elegiste el 1° ejercicio----------");
+                pilaEntero.makeEmpty();
                 ejercicio1();
                 break;
             case 2:
                 System.out.println("----------Elegiste el 2° ejercicio----------");
+                pilaString.makeEmpty();
                 ejercicio2();
                 break;
             case 3:
                 System.out.println("----------Elegiste el 3° ejercicio----------");
+                pilaString.makeEmpty();
                 ejercicio3();
                 break;
             case 4:
                 System.out.println("----------Elegiste el 4° ejercicio----------");
+                pilaEntero.makeEmpty();
                 ejercicio4();
                 break;
             case 5:
+                System.out.println("----------Elegiste el 5° ejercicio----------");
+                colaEntero.makeEmpty();
+                ejercicio5();
+                break;
+            case 6:
+                System.out.println("----------Elegiste el 6° ejercicio----------");
+                colaEntero.makeEmpty();
+                ejercicio6();
+                break;
+            case 7:
+                System.out.println("----------Elegiste el 7° ejercicio----------");
+                colaEntero.makeEmpty();
+                ejercicio7();
+                break;
+            case 8:
+                System.out.println("----------Elegiste el 8° ejercicio----------");
+                ejercicio8();
+                break;
+            case 9:
                 System.out.println("Adios");
                 break;
             default:
                 System.out.println("Opcion no valida");
                 break;
         }
-    }while(op!=5);
+    }while(op!=9);
     
 }
    private void ejercicio1(){
@@ -109,7 +138,7 @@ public class EjercicioPilasColasconLista {
         }
     }
 
-    public void ejercicio4(){
+    private void ejercicio4(){
         System.out.print("Ingresa un número decimal: ");
         int numeroDecimal = consola.nextInt();
 
@@ -129,6 +158,108 @@ public class EjercicioPilasColasconLista {
                 System.out.print(pilaEntero.topAndPop()); // Mostramos el elemento en la cima y lo removemos
             }
             System.out.println();
+        }
     }
-}
+
+    private void ejercicio5(){
+        do {
+            System.out.println("Ingresa una opcion:");
+            System.out.println("1-Agregar elementos");
+            System.out.println("2-Eliminar elemento");
+            System.out.println("3-Mostrar el frente");
+            System.out.println("4-Mostrar cola");
+            System.out.println("5-Salir");
+            op2=consola.nextInt();
+            switch (op2) {
+                case 1:
+                    System.out.println("Ingresa el elemento para agregar:");
+                    x=consola.nextInt();
+                    colaEntero.enqueue(x);
+                    break;
+                case 2:
+                    System.out.println("Numero "+colaEntero.dequeue()+" eliminado");
+                    break;
+                case 3:
+                    System.out.println("El frente es: "+colaEntero.getFront());
+                    break;
+                case 4: 
+                    colaEntero.mostrarCola();
+                    break;
+            }
+        } while (op2!=5);
+    }
+
+    private void ejercicio6(){
+        System.out.println("Ingresa la cantidad de elementos que deseas agregar a la cola: ");
+        int cant = consola.nextInt();
+        System.out.println("Ingresa los datos: ");
+        for(int i=0; i<cant; i++){
+            int x = consola.nextInt();
+            colaEntero.enqueue(x);
+        }
+         // Proceso de desencolado y suma
+         int sumaTotal = 0;
+         System.out.println("\nDesencolando y sumando números...");
+         while (!colaEntero.isEmpty()) {
+             int numero = colaEntero.dequeue();
+             sumaTotal += numero;
+             System.out.println("Desencolado: " + numero);
+         }
+         
+         System.out.println("La suma total de los números es: " + sumaTotal);
+    }
+
+    private void ejercicio7(){
+        Pila<Integer> pilaAuxiliar = new Pila<>();
+
+        System.out.println("Ingresa la cantidad de elementos que deseas agregar a la cola: ");
+        int cant = consola.nextInt();
+        System.out.println("Ingresa los datos: ");
+        for(int i=0; i<cant; i++){
+            int x = consola.nextInt();
+            colaEntero.enqueue(x);
+        }
+
+        while (!colaEntero.isEmpty()) {
+            // Extrae el primer elemento de la cola
+            int temp = colaEntero.dequeue();
+
+            // Mueve los elementos de la pila a la cola hasta encontrar el lugar adecuado para 'temp'
+            while (!pilaAuxiliar.isEmpty() && pilaAuxiliar.top() > temp) {
+                colaEntero.enqueue(pilaAuxiliar.topAndPop());
+            }
+
+            // Inserta el elemento en la pila
+            pilaAuxiliar.push(temp);
+        }
+
+        // Pasar los elementos ordenados de la pila de nuevo a la cola
+        while (!pilaAuxiliar.isEmpty()) {
+            colaEntero.enqueue(pilaAuxiliar.topAndPop());
+        }
+
+        System.out.println("Cola ordenada: ");
+        colaEntero.mostrarCola();
+    }
+
+    private void ejercicio8(){
+        Cola<Character> colaNormal = new Cola<>();
+        Cola<Character> colaInversa = new Cola<>();
+        System.out.println("Ingresa una palabra o frase:");
+        String palabra = consola.nextLine();
+        // Normalizar la frase: convertir a minúsculas, quitar espacios y signos de puntuación
+        palabra = palabra.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+        //ingresar cada letra a una cola
+        for(int i = 0; i < palabra.length();i++){
+            colaNormal.enqueue(palabra.charAt(i));
+        }
+        
+        //ingresar cada letra pero con la palabra inversa
+        for(int i = palabra.length(); i < 0; i--){
+            colaInversa.enqueue(palabra.charAt(i));
+        }
+
+        
+    }
 }
