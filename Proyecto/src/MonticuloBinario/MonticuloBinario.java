d4d9dbda0dfb870b6cd2264f5a9affa33098ce0e
Francisco Bossio <franciscobossio10@gmail.com>
2024-11-07 20:58:22 -0300

    archivos actualizados

--- Proyecto/src/MonticuloBinario/MonticuloBinario.java
+++ Proyecto/src/MonticuloBinario/MonticuloBinario.java
@@ -2,80 +2,83 @@ package MonticuloBinario;
 import java.util.NoSuchElementException;
 
 public class MonticuloBinario {
-    private static final int DEFAULT_CAPACITY=100;
+    private static final int DEFAULT_CAPACITY = 100;
     private int currentSize;
-    private int [] array;
+    private int[] array;
 
-    public MonticuloBinario(){
-        currentSize=0;
-        array=(int []) new int[DEFAULT_CAPACITY+1];
+    public MonticuloBinario() {
+        currentSize = 0;
+        array = new int[DEFAULT_CAPACITY + 1];
     }
 
-    //retorna el menor valor del arreglo
-    public int buscar_minimo(){
-        if(isEmpty()){
+    // Retorna el menor valor del arreglo
+    public int buscar_minimo() {
+        if (isEmpty()) {
             throw new NoSuchElementException();
-        }else{
+        } else {
             return array[1];
         }
     }
 
-    //pregunta si el arreglo esta vacio
-    public boolean isEmpty(){
-        return currentSize==0;
+    // Pregunta si el arreglo está vacío
+    public boolean isEmpty() {
+        return currentSize == 0;
     }
 
-    //agrega un numero al arreglo
-    public void agregar(int n){
-        if(currentSize + 1 == array.length ){
+    // Agrega un número al arreglo
+    public void agregar(int n) {
+        if (currentSize + 1 == array.length) {
             doubleArray();
-        }else{
-            int pos=currentSize++;
-            for(;n<array[pos/2] && pos>=1; pos/=2){
-                array[pos]=array[pos/2];
-            }
-            array[pos]=n;
-            currentSize++;
         }
+        int pos = ++currentSize;
+        while (pos > 1 && n < array[pos / 2]) {
+            array[pos] = array[pos / 2];
+            pos /= 2;
+        }
+        array[pos] = n;
     }
 
-    //eliminar el minimo
-    public int eliminar(){
-        int minimo=buscar_minimo();
-        array[1]=array[currentSize--];
+    // Eliminar el mínimo
+    public int eliminar() {
+        int minimo = buscar_minimo();
+        array[1] = array[currentSize--];
         percolateDown(1);
         return minimo;
     }
 
-    private void percolateDown(int pos){
+    private void percolateDown(int pos) {
         int child;
-        int aux=array[pos];
-        for(;pos*2<=currentSize;pos=child){
-            child=pos*2;
-            if(child != currentSize && array[child+1]<array[child]){
+        int aux = array[pos];
+        while (pos * 2 <= currentSize) {
+            child = pos * 2;
+            if (child != currentSize && array[child + 1] < array[child]) {
                 child++;
             }
-            if(array[child]<aux){
-                array[pos]=array[child];
-            }else{
+            if (array[child] < aux) {
+                array[pos] = array[child];
+            } else {
                 break;
             }
-            array[pos]=aux;
+            pos = child;
         }
+        array[pos] = aux;
     }
 
-    //imprimir el arreglo
-    public void imprimir(){
-        if(isEmpty()){
+    // Imprimir el arreglo
+    public void imprimir() {
+        if (isEmpty()) {
             throw new NoSuchElementException();
-        }else{
-            for (int i=1;i<currentSize+1;i++) {
+        } else {
+            for (int i = 1; i <= currentSize; i++) {
                 System.out.println(array[i]);
             }
         }
-        
     }
-    private void doubleArray(){
-        array=(int []) new int[array.length*2];
+
+    private void doubleArray() {
+        int[] newArray = new int[array.length * 2];
+        System.arraycopy(array, 1, newArray, 1, currentSize);
+        array = newArray;
     }
 }
+
