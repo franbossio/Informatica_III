7ad5f7022b279533118ec24903f3486598ee4d20
Francisco Bossio <franciscobossio10@gmail.com>
2024-11-10 13:32:29 -0300

    archivos actualizados

--- Proyecto/src/ArbolRojinegro/EjercicioRojinegro.java
+++ Proyecto/src/ArbolRojinegro/EjercicioRojinegro.java
@@ -3,18 +3,18 @@ package ArbolRojinegro;
 import java.util.Scanner;
 
 public class EjercicioRojinegro {
-    public void ejecutar(){
+    public void ejecutar() {
         ArbolRojinegro arbolRojinegro = new ArbolRojinegro();
-        Scanner consola=new Scanner(System.in);
-        int op=0;
-        do{
+        Scanner consola = new Scanner(System.in);
+        int op = 0;
+        do {
             System.out.println("----------Arbol Rojinegro----------");
             System.out.println("Seleccione una opcion:");
             System.out.println("1. Inserción en un Árbol Rojinegro y Eliminación en un Árbol Rojinegro");
             System.out.println("2. Verificación de Propiedades y Altura Negra");
             System.out.println("3-Salir");
-            op=consola.nextInt();
-            switch(op){
+            op = consola.nextInt();
+            switch (op) {
                 case 1:
                     System.out.println("----------Elegiste el 1° ejercicio----------");
                     do {
@@ -23,7 +23,7 @@ public class EjercicioRojinegro {
                         System.out.println("2-Eliminar un elemento");
                         System.out.println("3-Imprimir el arbol");
                         System.out.println("4-Salir");
-                        op=consola.nextInt();
+                        op = consola.nextInt();
                         switch (op) {
                             case 1:
                                 System.out.println("Ingresa un elemento:");
@@ -33,27 +33,29 @@ public class EjercicioRojinegro {
                             case 2:
                                 System.out.println("Ingrese el elemento a eliminar:");
                                 int eliminar = consola.nextInt();
-                                
+
                                 break;
                             case 3:
-                                
+
                                 break;
                             case 4:
                                 System.out.println("Adios");
                                 break;
                         }
-                    } while (op!=4);
+                    } while (op != 4);
                     break;
                 case 2:
-                    System.out.println("----------Elegiste el 1° ejercicio----------");
+                    System.out.println("----------Elegiste el 2° ejercicio----------");
+                    System.out.println("El arbol cumple con las propiedades rojinegras: "
+                            + arbolRojinegro.verificarPropiedadesRojinegras());
                     break;
                 case 3:
                     System.out.println("Adios");
                     break;
                 default:
                     System.out.println("La opcion ingresada no es correcta");
-                    break;       
+                    break;
             }
-        }while(op!=3); 
+        } while (op != 3);
     }
 }
